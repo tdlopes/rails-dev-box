81ae179dbc251133a3e85e6aab9e6051e6f72b4f
Tiago Lopes <tiago.delfim.lopes@gmail.com>
2017-11-28 13:10:12 +0000

    Fix setup ruby script to edit .profile instead of .bashrc

--- setup-ruby.sh
+++ setup-ruby.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.profile
 source ~/.profile
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.profile
